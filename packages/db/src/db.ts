553825c6cd0d1bbedf67b07698eb5b2dff027682
Mehul Prajapati <ailearn90@gmail.com>
2026-02-23 05:09:25 +0530

    refactor: added monorepo using turborepo

---
+++ packages/db/src/db.ts
@@ -0,0 +1,12 @@
+import "dotenv/config";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "@prisma/client";
+import { env } from "./config/env";
+
+const connectionString = env.DATABASE_URL;
+if (!connectionString) {
+    throw new Error("DATABASE_URL is not set");
+}
+
+const adapter = new PrismaPg({ connectionString });
+export const client = new PrismaClient({ adapter });
