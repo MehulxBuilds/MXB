80d3d475f9c7681a79b64ac465369a92b2bba4d6
Mehul Prajapati <ailearn90@gmail.com>
2026-02-23 05:10:18 +0530

    refactor: added monorepo using turborepo

---
+++ apps/web/src/app/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className="w-full flex justify-center items-center p-4">
+      <h2 className="flex gap-2 text-lg font-medium font-mono">This Domain is owned by
+        <span>
+          <a href="https://x.com/MehulxBuilds" target="_blank" className="text-blue-500 hover:underline">Mehul
+          </a>
+        </span>
+      </h2>
+    </div>
+  )
+}
+
+export default page;
