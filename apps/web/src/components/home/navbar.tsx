e60837f94557a191c45ea4aa3f70bea9db081f6a
Mehul Prajapati <ailearn90@gmail.com>
2026-04-15 01:40:52 +0530

    fix: fixed the eslint issue in repo - v1 0.0.4

--- apps/web/src/components/home/navbar.tsx
+++ apps/web/src/components/home/navbar.tsx
@@ -25,7 +25,7 @@ const Navbar = ({ onSectionScroll }: NavbarType) => {
                     onClick={() => onSectionScroll("contacts")}
                     className="flex items-center gap-1.5 font-semibold text-gray-900 hover:text-black transition-colors group cursor-pointer hover:underline"
                 >
-                    Let's Connect
+                    {"Let's Connect"}
                     <MoveUpRight className="size-3.5 transition-transform group-hover:translate-x-0.5 group-hover:-translate-y-0.5" />
                 </div>
             </div>
